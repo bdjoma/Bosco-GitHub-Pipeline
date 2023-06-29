d19d0a1a527e2cf1e6a3a4aac14ee86f80d133db
bdjoma <bdjomatchoua@gmail.com>
2023-06-29 19:51:03 -0400

    added env variables for credentials

--- variables.tf
+++ variables.tf
@@ -3,15 +3,3 @@ variable "my-region" {
   type        = string
   default     = "us-east-1"
 }
-
-variable "my-access-key" {
-  description = "this is the access key"
-  type        = string
-  default     = "AKIAXBHD4ENHEUJTG4E6"
-}
-
-variable "my-secret-key" {
-  description = "this is the secret key"
-  type        = string
-  default     = "KyQ9qhXUyx4y1HuA+02VarlaqNGDSUvDdc0za4nO"
-}
