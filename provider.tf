e5cfd012827afe5f055fb67c8cf0b19816aadae9
bdjoma <bdjomatchoua@gmail.com>
2023-06-29 21:27:01 -0400

    added backend state

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region     = var.my-region
   }
+
+terraform {
+
+  backend "s3" {
+    bucket = "bucket-gh-terraform-state"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
